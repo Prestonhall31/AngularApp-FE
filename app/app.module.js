2cf31bf4ae41af13f036c87b843baef3eac904cc
prestonhall31 <prestonhall31@gmail.com>
2017-03-06 20:23:36 -0700

    Added proposals show, show and list

--- app/app.module.js
+++ app/app.module.js
@@ -5,6 +5,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
@@ -13,6 +14,7 @@ var homepage_component_1 = require("./homepage/homepage.component");
 var documents_component_1 = require("./documents/documents.component");
 var proposal_list_component_1 = require("./proposal/proposal-list.component");
 var proposal_new_component_1 = require("./proposal/proposal-new.component");
+var proposal_show_component_1 = require("./proposal/proposal-show.component");
 var app_routing_module_1 = require("./app-routing.module");
 var AppModule = (function () {
     function AppModule() {
@@ -32,6 +34,7 @@ AppModule = __decorate([
             documents_component_1.DocumentsComponent,
             proposal_list_component_1.ProposalListComponent,
             proposal_new_component_1.ProposalNewComponent,
+            proposal_show_component_1.ProposalShowComponent,
         ],
         bootstrap: [
             app_component_1.AppComponent
